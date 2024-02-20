cca6f1899d035d627e2216ac276adc1ed62b4aa2
jucamo0713 <jucamo.0713@gmail.com>
2024-02-20 12:58:46 -0500

    orchesting

--- word_count.py
+++ word_count.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 def load_input(input_directory):
     """Load text files in 'input_directory/'"""
+    pass
     #
     # Lea los archivos de texto en la carpeta input/ y almacene el contenido en
     # un DataFrame de Pandas. Cada línea del archivo de texto debe ser una
@@ -15,6 +16,7 @@ def load_input(input_directory):
 
 
 def clean_text(dataframe):
+    pass
     """Text cleaning"""
     #
     # Elimine la puntuación y convierta el texto a minúsculas.
@@ -22,10 +24,12 @@ def clean_text(dataframe):
 
 
 def count_words(dataframe):
+    pass
     """Word count"""
 
 
 def save_output(dataframe, output_filename):
+    pass
     """Save output to a file."""
 
 
@@ -33,7 +37,10 @@ def save_output(dataframe, output_filename):
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
 def run(input_directory, output_filename):
-
+    dataframe = load_input(input_directory)
+    dataframe = clean_text(dataframe)
+    dataframe = count_words(dataframe)
+    save_output(dataframe, output_filename)
 
 if __name__ == "__main__":
     run(
