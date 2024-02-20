a3a253e3bc66462ce424c84546bf0c5e2f0f61aa
jucamo0713 <jucamo.0713@gmail.com>
2024-02-20 13:41:28 -0500

    load and clean

--- word_count.py
+++ word_count.py
@@ -1,34 +1,42 @@
 """Taller evaluable"""
 
 import glob
+import re
 
 import pandas as pd
+from pandas import DataFrame
 
 
-def load_input(input_directory):
-    """Load text files in 'input_directory/'"""
-    pass
-    #
-    # Lea los archivos de texto en la carpeta input/ y almacene el contenido en
-    # un DataFrame de Pandas. Cada línea del archivo de texto debe ser una
-    # entrada en el DataFrame.
-    #
+def load_input(input_directory: str) -> DataFrame:
+    filenames = glob.glob(input_directory + '/*.*')
+    dataframes = [
+        pd.read_csv(filename, sep='§', index_col=None, names=['text'])
+        for
+        filename in filenames
+    ]
+    dataframe = pd.concat(dataframes).reset_index(drop=True)
+    return dataframe
 
 
-def clean_text(dataframe):
-    pass
+def clean_text(dataframe: DataFrame):
     """Text cleaning"""
     #
     # Elimine la puntuación y convierta el texto a minúsculas.
     #
 
+    dataframe = dataframe.copy()
+    dataframe = dataframe['text'].replace(r'[\.,\/#!$%\^&\*;:{}=\-_`~()\['
+                                          r'\]]', '',
+                                          regex=True).apply(str.lower).apply(
+        str.strip)
+    return dataframe
 
-def count_words(dataframe):
+
+def count_words(dataframe: DataFrame) -> DataFrame:
     pass
-    """Word count"""
 
 
-def save_output(dataframe, output_filename):
+def save_output(dataframe: DataFrame, output_filename: str) -> None:
     pass
     """Save output to a file."""
 
@@ -39,8 +47,10 @@ def save_output(dataframe, output_filename):
 def run(input_directory, output_filename):
     dataframe = load_input(input_directory)
     dataframe = clean_text(dataframe)
+    print(dataframe)
     dataframe = count_words(dataframe)
     save_output(dataframe, output_filename)
+
 
 if __name__ == "__main__":
     run(
